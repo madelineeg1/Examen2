ca42c7a95bf270ff4dfb9c502130d7709ad7f4c1
Madeline <madelineelizondogutierrez123@gmail.com>
2023-11-28 09:15:54 -0600

    Clase Pacientes
    Esta es la clase Pacientes

---
+++ src/Pacientes/CrudPacientes.java
@@ -0,0 +1,41 @@
+
+package Pacientes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Student
+ */
+public class CrudPacientes extends ClasePacientes{
+ ArrayList<Integer> CrudPacientes = new ArrayList<>();
+ 
+ 
+ public void agregarPaciente(){
+ 
+    
+    
+}
+ 
+public void ClasificacionPacientes(){
+    
+    
+}
+
+public void listaAtendidos(){
+    
+    
+    
+    
+}
+        
+ public void buscarPaciente(){
+     
+     
+ }
+ 
+    
+    
+    
+    
+}
